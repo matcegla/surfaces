fbd00513fed499fe79552a19fe72cea80956283c
Mateusz Cegiełka <mateusz@cegla.net>
2018-11-24 17:09:33 +0100

    vector based physics

--- main.cpp
+++ main.cpp
@@ -9,9 +9,11 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <stb_image.h>
+#define GLM_ENABLE_EXPERIMENTAL
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/vector_angle.hpp>
 
 struct Log {
 	template<typename... Ts> void error(Ts&&... xs) { std::cout << "\033[1;31merror:\033[0m "; (std::cout << ... << xs); }
@@ -36,13 +38,10 @@ struct Shader {
     void free() { glDeleteShader(id); }
 };
 struct Uniform {
-#pragma clang diagnostic push
-#pragma ide diagnostic ignored "cppcoreguidelines-c-copy-assignment-signature"
-    void operator=(float x) { glUniform1f(id, x); }
-    void operator=(int x) { glUniform1i(id, x); }
-    void operator=(const glm::mat4& x) { glUniformMatrix4fv(id, 1, GL_FALSE, glm::value_ptr(x)); }
-    void operator=(const glm::vec3& x) { glUniform3f(id, x.x, x.y, x.z); }
-#pragma clang diagnostic pop
+    void operator=(float x) { glUniform1f(id, x); } // NOLINT(misc-unconventional-assign-operator)
+    void operator=(int x) { glUniform1i(id, x); } // NOLINT(misc-unconventional-assign-operator)
+    void operator=(const glm::mat4& x) { glUniformMatrix4fv(id, 1, GL_FALSE, glm::value_ptr(x)); } // NOLINT(misc-unconventional-assign-operator)
+    void operator=(const glm::vec3& x) { glUniform3f(id, x.x, x.y, x.z); } // NOLINT(misc-unconventional-assign-operator)
 	int id;
 };
 struct Program {
@@ -161,13 +160,20 @@ struct ScreenInfo {
 	int width, height;
 };
 
-template <typename T, unsigned n> constexpr unsigned arraySize(T(&)[n]) { return n; }
 std::string readFile(const std::string& path) {
     std::ostringstream oss;
     std::ifstream file(path);
     file >> oss.rdbuf();
     return oss.str();
 }
+glm::vec3 rgb(int r, int g, int b) { return glm::vec3((float)r / 255, (float)g / 255, (float)b / 255); }
+float clamp(float a, float b, float x) { return std::min(std::max(x, a), b); }
+float sgn(float x) { return x < 0 ? -1 : x > 0 ? 1 : 0; }
+float mix(float a, float b, float x) { return x * a + (1 - x) * b; }
+std::minstd_rand rng((unsigned long)std::chrono::high_resolution_clock::now().time_since_epoch().count()); // NOLINT(cert-err58-cpp)
+float uniform(float a, float b) { return std::uniform_real_distribution<float>(a, b)(rng); }
+glm::vec2 enorm(glm::vec2 v) { return glm::length(v) == 0.0f ? glm::vec2() : glm::normalize(v); }
+namespace glm { std::ostream &operator<<(std::ostream &out, glm::vec<2, float, glm::packed_highp> v) { return out << v.x << ',' << v.y; } }
 
 Shader shaderFromFile(const std::string& path, GLenum type) {
 	auto src = readFile(path);
@@ -206,13 +212,11 @@ void loadGLAD() {
 		std::exit(-1);
 	}
 }
-glm::vec3 rgb(int r, int g, int b) { return glm::vec3((float)r / 255, (float)g / 255, (float)b / 255); }
-float clamp(float a, float b, float x) { return std::min(std::max(x, a), b); }
-float sgn(float x) { return x < 0 ? -1 : x > 0 ? 1 : 0; }
-float mix(float a, float b, float x) { return x * a + (1 - x) * b; }
 
-std::minstd_rand rng((unsigned long)std::chrono::high_resolution_clock::now().time_since_epoch().count()); // NOLINT(cert-err58-cpp)
-template <typename T> T uniform(T a, T b) { return std::uniform_real_distribution<T>(a, b)(rng); }
+const glm::vec3 gravity = {0.0f, -9.80665, 0.0f}; // NOLINT(cert-err58-cpp)
+const float waterDensity = 998.23; // kg/m^3
+const float airDensity = 1.225; // kg/m^3
+const float woodDensity = 600.0f;
 
 struct CameraFPS {
 	glm::vec3 pos, up, front;
@@ -260,7 +264,7 @@ struct Water {
 	Uniform utime, upv, umodel, uviewpos;
 	std::vector<float> vertices;
 	std::vector<unsigned> indices;
-	Water(unsigned width, unsigned depth, const std::string& vertPath, const std::string& fragPath, glm::vec3 sunPos):
+	Water(int width, int depth, const std::string& vertPath, const std::string& fragPath, glm::vec3 sunPos):
 		vao(),
 		vbo(),
 		ebo(),
@@ -270,7 +274,7 @@ struct Water {
 		umodel(shader.locateUniform("trans_model")),
 		uviewpos(shader.locateUniform("view_pos")),
 		vertices(),
-		indices(2*3*width*depth)
+		indices((unsigned)2*3*width*depth)
 	{
 		for (auto x=0; x<width+1; ++x) {
 			for (auto z=0; z<depth+1; ++z) {
@@ -308,6 +312,13 @@ struct Water {
 		glDrawElements(GL_TRIANGLES, (unsigned)indices.size() / (transparent ? 2 : 1), GL_UNSIGNED_INT, nullptr);
 		glBindVertexArray(0);
 	}
+	static float waveHeightAtPoint(glm::vec3 vertex_pos, float time) {
+		float wavePresence = (sinf((vertex_pos.x + vertex_pos.z + time)/16) + 1) / 2;
+		float x = vertex_pos.z / 8 + vertex_pos.x / 32 + time / 2;
+		float height = 4 * wavePresence * (sinf(x) + sinf(2*x) + sinf(3*x));
+//		float height = 0;
+		return height;
+	}
 };
 struct ToggleButton {
 	bool state;
@@ -324,12 +335,6 @@ struct ToggleButton {
 	}
 	bool operator*() { return state; }
 };
-const glm::vec3 gravity = {0.0f, -9.80665, 0.0f}; // NOLINT(cert-err58-cpp)
-const float waterDensity = 998.23; // kg/m^3
-const float airDensity = 1.225; // kg/m^3
-const float woodDensity = 600.0f;
-const float plateDragCoefficient = 1.17;
-
 struct CubeVertices {
 	VBO vbo;
 	VAO vao;
@@ -341,7 +346,7 @@ struct CubeVertices {
 	}
 	void draw() {
 		vao.bind();
-		glDrawArrays(GL_TRIANGLES, 0, arraySize(rawData));
+		glDrawArrays(GL_TRIANGLES, 0, sizeof(rawData)/sizeof(rawData[0]));
 		glBindVertexArray(0);
 	}
 	static constexpr float rawData[] = {
@@ -388,61 +393,81 @@ struct CubeVertices {
 		-0.5f, 0.5f, -0.5f,
 	};
 };
-
-struct Raft {
-	CubeVertices& cubev;
-	Program shader;
-	Uniform upv, umodel;
+struct RaftPhysics {
 	glm::vec3 position;
-	glm::vec3 velocity;
+	glm::vec2 velocity;
 	glm::vec3 scale;
 	float rotation;
 	float angularVelocity;
 	float mass;
-	Raft(glm::vec3 position, float mass, glm::vec3 scale, const std::string& vertPath, const std::string& fragPath, CubeVertices& cubev):
-		cubev(cubev),
-		shader(shaderProgramFromFiles(vertPath, fragPath)),
-		upv(shader.locateUniform("trans_pv")),
-		umodel(shader.locateUniform("trans_model")),
+	RaftPhysics(glm::vec3 position, glm::vec3 scale, float mass):
 		position(position),
 		velocity(),
 		scale(scale),
 		rotation(0.0f),
 		angularVelocity(0.0f),
 		mass(mass)
-	{
-	}
-
-	static float waveHeightAtPoint(glm::vec3 vertex_pos, float time) {
-		float wavePresence = (sinf((vertex_pos.x + vertex_pos.z + time)/16) + 1) / 2;
-		float x = vertex_pos.z / 8 + vertex_pos.x / 32 + time / 2;
-		float height = 4 * wavePresence * (sinf(x) + sinf(2*x) + sinf(3*x));
-//		float height = 0;
-		return height;
-	}
-	static glm::vec3 forceAtPoint(glm::vec3 position, float velocity, glm::vec3 scale, float mass, float time) {
-		auto waveHeight = waveHeightAtPoint(position, time);
+	{}
+	static glm::vec2 map2d(glm::vec3 v) { return glm::vec2(v.z, v.y); }
+	static glm::vec2 forceAtPoint(glm::vec3 position, glm::vec2 velocity, glm::vec3 scale, float rotation, float mass, float time) {
+		auto waveHeight = Water::waveHeightAtPoint(position, time);
 		auto lowerEdgeY = position.y - scale.y / 2;
 		auto submergedHeight = clamp(0.0f, scale.y, waveHeight - lowerEdgeY);
-		auto displacedWaterVolume = submergedHeight * scale.x * scale.z;
-		auto averageFluidDensity = mix(waterDensity, airDensity, submergedHeight / scale.y);		
 		auto area = scale.x * scale.z;
-		auto up = glm::vec3(0.0f, 1.0f, 0.0f);
+		auto displacedWaterVolume = area * submergedHeight;
 
-		auto weight = mass * gravity;
-		auto buoyancy = - waterDensity * displacedWaterVolume * gravity;
-		auto drag = -sgn(velocity) * up * 0.5f * averageFluidDensity * powf(velocity, 2) * plateDragCoefficient * area;
+		auto weight = mass * map2d(gravity);
+		auto buoyancy = - waterDensity * displacedWaterVolume * map2d(gravity);
+		auto drag = dragAtPoint(position, velocity, scale, rotation, time);
 
 		auto total = weight + buoyancy + drag;
 		return total;
 	}
-	static float dragAtPoint(glm::vec3 position, float velocity, glm::vec3 scale, float mass, float time) {
-		auto waveHeight = waveHeightAtPoint(position, time);
+	static glm::vec2 dragAtPoint(glm::vec3 position, glm::vec2 velocity, glm::vec3 scale, float rotation, float time) {
+		auto waveHeight = Water::waveHeightAtPoint(position, time);
 		auto fluidDensity = position.y > waveHeight ? airDensity : waterDensity;
-		auto area = scale.x * scale.z;
-		auto drag = -sgn(velocity) * 0.5f * fluidDensity * powf(velocity, 2) * plateDragCoefficient * area;
+		static const std::pair<float, float> inclinedCoefficients[] = {
+				{ 0.0f, 0.1f},
+				{25.0f, 0.4f},
+				{35.0f, 0.7f},
+				{40.0f, 0.6f},
+				{45.0f, 0.7f},
+				{50.0f, 0.8f},
+				{55.0f, 0.85f},
+				{60.0f, 0.9f},
+				{70.0f, 1.0f},
+				{80.0f, 1.1f},
+				{90.0f, 1.1f},
+		}; // http://www.iawe.org/Proceedings/BBAA7/X.Ortiz.pdf
+		auto relangle = [&]{
+			auto normed = fmodf(fmodf(rotation - glm::angle(velocity, glm::vec2(1.0f, 0.0f)), 2*M_PIf32) + 2*M_PIf32, 2*M_PIf32);
+			if (normed < M_PI_2f32)
+				return normed;
+			else if (normed < M_PI)
+				return M_PIf32 - normed;
+			else if (normed < 1.5f * M_PIf32)
+				return normed - M_PIf32;
+			else
+				return 2*M_PIf32 - normed;
+		}();
+		auto relh = scale.z * sinf(relangle);
+		auto relarea = scale.x * relh;
+		// TODO check if angles are correct
+		auto dragCoefficient = [&]{
+			for (auto i=0; i+1<(int)sizeof(inclinedCoefficients)/(int)sizeof(inclinedCoefficients[0]); ++i) {
+				auto degrelangle = glm::degrees(relangle);
+				auto datum1 = inclinedCoefficients[i];
+				auto datum2 = inclinedCoefficients[i+1];
+				if (datum1.first <= degrelangle and degrelangle <= datum2.first)
+					return mix(datum1.second, datum2.second, (degrelangle - datum1.first) / (datum2.first - datum1.first));
+			}
+			return 0.0f;
+		}();
+		auto drag = -enorm(velocity) * 0.5f * fluidDensity * powf(glm::length(velocity), 2) * dragCoefficient * relarea;
 		return drag;
 	}
+	static glm::vec2 rotate90(glm::vec2 v) { return glm::vec2(-v.y, v.x); }
+
 	float computeAngularAcceleration(float time) {
 		auto halfScale = scale;
 		halfScale.z /= 2;
@@ -453,33 +478,50 @@ struct Raft {
 		rightPos.z += scale.z / 4 * cosf(rotation);
 		rightPos.y += scale.z / 4 * sinf(rotation);
 
-		auto leftForce = forceAtPoint(leftPos, velocity.y, halfScale, mass/2, time).y;
-		auto rightForce = forceAtPoint(rightPos, velocity.y, halfScale, mass/2, time).y;
-		auto rightAngDrag = dragAtPoint(rightPos, angularVelocity * scale.z / 4, halfScale, mass/2, time);
-		auto leftAngDrag = dragAtPoint(leftPos, angularVelocity * scale.z / 4, halfScale, mass/2, time);
+		auto v2d = glm::vec2(0.0f, velocity.y);
+		auto leftVel = v2d + angularVelocity * (scale.z/4) * enorm(rotate90(map2d(leftPos - position)));
+		auto rightVel = v2d + angularVelocity * (scale.z/4) * enorm(rotate90(map2d(rightPos - position)));
 
-		auto rightTorque = (rightForce * cosf(rotation) + rightAngDrag) * (scale.z / 4);
-		auto leftTorque = (leftForce * cosf(-rotation) - leftAngDrag) * (scale.z / 4);
-		auto torque = rightTorque - leftTorque;
+		auto leftForce = forceAtPoint(leftPos, leftVel, halfScale, rotation, mass/2, time);
+		auto rightForce = forceAtPoint(rightPos, rightVel, halfScale, rotation, mass/2, time);
 
+		auto leftTorque = (- leftForce.y * cosf(rotation) + leftForce.x * sinf(rotation)) * (scale.z / 4);
+		auto rightTorque = (- rightForce.x * sinf(rotation) + rightForce.y * cosf(rotation)) * (scale.z / 4);
+
+		auto torque = leftTorque + rightTorque;
 		auto momentOfInertia = (1.0f / 12) * mass * (powf(scale.z, 2) + powf(scale.y, 2));
 		auto angularAcceleration = torque / momentOfInertia;
-
 		return angularAcceleration;
 	}
 	void update(float deltaTime, float time) {
-		auto force = forceAtPoint(position, velocity.y, scale, mass, time);
+		auto force = forceAtPoint(position, glm::vec2(0.0f, velocity.y), scale, rotation, mass, time);
 		auto angularAcceleration = computeAngularAcceleration(time);
 		velocity += deltaTime * force / mass;
-		position += deltaTime * velocity;
+		position += deltaTime * glm::vec3(0.0f, velocity.y, velocity.x);
 		angularVelocity += deltaTime * angularAcceleration;
 		rotation += deltaTime * angularVelocity;
 	}
+};
+struct Raft {
+	CubeVertices& cubev;
+	Program shader;
+	Uniform upv, umodel;
+	RaftPhysics physics;
+	Raft(glm::vec3 position, float mass, glm::vec3 scale, const std::string& vertPath, const std::string& fragPath, CubeVertices& cubev):
+		cubev(cubev),
+		shader(shaderProgramFromFiles(vertPath, fragPath)),
+		upv(shader.locateUniform("trans_pv")),
+		umodel(shader.locateUniform("trans_model")),
+		physics(position, scale, mass)
+	{}
+	void update(float deltaTime, float time) {
+		physics.update(deltaTime, time);
+	}
 	void draw(const glm::mat4& transPV) {
 		auto model = glm::mat4(1.0f);
-		model = glm::translate(model, position);
-		model = glm::rotate(model, -rotation, glm::vec3(1.0f, 0.0f, 0.0f));
-		model = glm::scale(model, scale);
+		model = glm::translate(model, physics.position);
+		model = glm::rotate(model, -physics.rotation, glm::vec3(1.0f, 0.0f, 0.0f));
+		model = glm::scale(model, physics.scale);
 		shader.use();
 		upv = transPV;
 		umodel = model;
@@ -512,7 +554,7 @@ struct Sun {
 };
 
 auto screen = ScreenInfo { 1920, 1080 };
-auto camera = CameraFPS({470.0f, 10.0f, 500.0f}); // NOLINT(cert-err58-cpp)
+auto camera = CameraFPS({470.0f, 5.0f, 500.0f}); // NOLINT(cert-err58-cpp)
 
 int main() {
 	auto glfw = GLFW();
@@ -539,7 +581,7 @@ int main() {
 	auto sun = Sun({550.0f, 30.0f, 550.0f}, {10.0f, 10.0f, 10.0f}, "standard.vert", "sun.frag", cubeVertices);
 	auto water = Water(1000, 1000, "water.vert", "water.frag", sun.position);
 	auto raftScale = glm::vec3(10.0f, 0.5f, 10.0f);
-	auto raft = Raft({500.0f, 6.0f, 500.0f}, raftScale.x * raftScale.y * raftScale.z * woodDensity, raftScale, "standard.vert", "raft.frag", cubeVertices);
+	auto raft = Raft({500.0f, 10.0f, 500.0f}, raftScale.x * raftScale.y * raftScale.z * woodDensity, raftScale, "standard.vert", "raft.frag", cubeVertices);
 
 	auto backgroundColor = rgb(0x00, 0x2b, 0x36);
 	auto lastTime = (float)glfw.time();
@@ -566,8 +608,9 @@ int main() {
             window.setShouldClose(true);
 		paused.update(window.getKey(GLFW_KEY_SPACE));
 		transparent.update(window.getKey(GLFW_KEY_F3));
-
 		camera.handleKeyboard(window.xkeyjoy(GLFW_KEY_D, GLFW_KEY_A), window.xkeyjoy(GLFW_KEY_E, GLFW_KEY_Q), window.xkeyjoy(GLFW_KEY_S, GLFW_KEY_W), cameraDeltaTime);
+
+		raft.update(physicsDeltaTime, physicsTime);
 
 		auto view = camera.viewMatrix();
 		auto projection = glm::perspective(glm::radians(45.0f), (float)screen.width / screen.height, 0.1f, 3000.0f);
@@ -579,7 +622,6 @@ int main() {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         water.draw(physicsTime, transPV, camera.pos, *transparent);
-        raft.update(physicsDeltaTime, physicsTime);
         raft.draw(transPV);
         sun.draw(transPV);
 
